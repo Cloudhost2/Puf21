e38c0aa853315585b2b38c48bc00722a08369348
PufferHosting <161731278+PufferHost@users.noreply.github.com>
2024-03-04 18:48:15 +0200

    Add files via upload

---
+++ js/addons.js
@@ -0,0 +1,11 @@
+document.addEventListener('keydown', function(event) {
+    if (event.key === '1') {
+        window.location.href = 'https://pufferhost.pl/';
+    }
+    if (event.key === '2') {
+        window.location.href = 'https://pufferhost.pl/about.html';
+    }
+    if (event.key === '3') {
+        window.location.href = 'https://pufferhost.pl/articles/index.html';
+    }
+});
